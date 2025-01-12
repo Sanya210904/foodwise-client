d648f3064a03f0d4b17364706ed710eb42805f1a
Sanya210904 <sanya210904@gmail.com>
2025-01-12 16:19:58 +0200

    add fonts, icons, colors, offsets vars

---
+++ src/app/styles/colors.ts
@@ -0,0 +1,31 @@
+export const colors = {
+  surfacePrimary: '#fff',
+  surfaceSecondary: '#f1f1f1',
+
+  textPrimary: '#000',
+  textSecondary: '#4B4B4B',
+  textInverted: '#fff',
+  textLink: '#5193C2',
+  textVariant: '#494949',
+
+  brandPrimaryFrom: '#6696ad',
+  brandPrimaryTo: '#bdd4d7',
+  brandDanger: '#D3393C',
+  brandSuccess: '#5dbc83',
+
+  buttonPrimaryFrom: '#6696ad',
+  buttonPrimaryTo: '#bdd4d7',
+  buttonSecondary: '#000',
+  buttonGrey: '#f1f1f1',
+  buttonWhite: '#fff',
+  buttonDisabled: '#d0d0d0',
+
+  borderPrimary: '#ebebeb',
+
+  cardPrimary: '#fff',
+
+  inputPrimary: '#f1f1f1',
+
+  navbarColor: '#fff',
+  statusBarColor: '#fff',
+};
---
+++ src/app/styles/offsets.ts
@@ -0,0 +1,5 @@
+export const offsets = {
+  containerOffsetHorizontal: 24,
+  containerOffsetTop: 24,
+  containerOffsetBottom: 12,
+};
